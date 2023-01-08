ca7201080f3bba6badcbf847fc42201071e34b0a
Kwon Sehoon <devkwonsehoon@naver.com>
2023-01-08 18:35:26 +0900

    update: UserDTO 업데이트 및 컨트롤러 단위에 Guard 설정

--- src/modules/v1/user/dto/user.dto.ts
+++ src/modules/v1/user/dto/user.dto.ts
@@ -1,6 +1,15 @@
 import { SocialPlatform } from '@modules/v1/auth/common/auth.type';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UserDTO {
   @ApiProperty({
@@ -10,17 +19,41 @@ export class UserDTO {
   @IsNumber()
   id: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '유저의 uuid 값입니다.',
+    required: true,
+  })
   @IsString()
   uuid: string;
 
-  @ApiProperty()
-  @IsString()
+  @ApiProperty({
+    description: '유저가 가입한 소셜 플랫폼 경로입니다.',
+    required: true,
+  })
+  @IsString({ message: '소셜 값은 문자여야 합니다.' })
+  @IsIn(['kakao', 'apple', 'google'], {
+    message: `소셜 값은 'kakao', 'apple', 'google'만 사용 가능합니다.`,
+  })
+  @IsNotEmpty({ message: '소셜 값은 필수입니다.' })
   provider: SocialPlatform;
 
-  @ApiProperty()
-  @IsString()
+  @ApiProperty({
+    description: '유저의 닉네임입니다.',
+    required: true,
+  })
+  @IsString({ message: '유저의 닉네임은 문자열이여야 합니다.' })
+  @MinLength(1, { message: '닉네임은 1자 이상 10자 이하이여야 합니다' })
+  @MaxLength(10, { message: '닉네임은 1자 이상 10자 이하이여야 합니다' })
   nickname: string;
+
+  @ApiProperty({
+    description: 'Refresh 토큰 값입니다.',
+    required: true,
+    example: 'refresh token here',
+  })
+  @IsNotEmpty()
+  @IsString()
+  refreshToken: string;
 
   @ApiProperty()
   @IsDate()
