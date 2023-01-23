4a0e32b7a4a2d6cd32f182e283ced58239b59f7d
Kwon Sehoon <devkwonsehoon@naver.com>
2023-01-23 14:34:56 +0900

    feat: Activity 레포지토리 인터페이스 추가

---
+++ src/modules/v1/activity/interfaces/activity-repository.interface.ts
@@ -0,0 +1,28 @@
+import BaseRepositoryInterface from '@common/interfaces/base-repository.interface';
+import { Activity } from '@prisma/client';
+
+export const ACTIVITY_REPOSITORY = 'ACTIVITY REPOSITORY';
+
+export interface ActivityRepositoryInterface
+  extends BaseRepositoryInterface<Activity> {
+  findByDate(userId: number, date: Date): Promise<Activity>;
+  findBetweenDateAndDate(
+    userId: number,
+    startDate: Date,
+    endDate: Date,
+  ): Promise<Activity[]>;
+  create(
+    userId: number,
+    characterId: number,
+    content: string,
+    date: Date,
+    image?: string,
+  ): Promise<Activity>;
+  update(
+    activityId: number,
+    content: string,
+    date: Date,
+    image?: string,
+  ): Promise<Activity>;
+  delete(activityId: number): Promise<void>;
+}
