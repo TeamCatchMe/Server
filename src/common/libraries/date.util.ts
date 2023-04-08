9226f18c8a6e4597b8b34e6ae3e7511a70116cd0
Kwon Sehoon <devkwonsehoon@naver.com>
2023-04-08 16:10:39 +0900

    update: DateUtil 내 validation 함수 수정

--- src/common/libraries/date.util.ts
+++ src/common/libraries/date.util.ts
@@ -1,13 +1,13 @@
 import dayjs from 'dayjs';
-
 export class DateUtil {
   static isValidDate(date: string): boolean {
-    if (dayjs(date).isValid()) return true;
-    return false;
+    if (!/^\d{4}\d{1,2}\d{1,2}$/.test(date)) return false;
+    if (!dayjs(date).isValid) return false;
+    return true;
   }
 
   static toDate(date: string): Date {
-    if (this.isValidDate(date)) return null;
+    if (!this.isValidDate(date)) return null;
     return dayjs(date, 'YYYYMMDD').add(9, 'h').toDate();
   }
 
